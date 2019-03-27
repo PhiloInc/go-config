9ad1e3106fce2f5a426d6f6bd287a37dea48ce4d
Richard Shaffer <richard@philo.com>
2019-03-26 18:42:08 -0700

    Make pointer setters work more like slice setters.
    Fixes a bug which makes String() always return "" for pointer setters.

--- ptr.go
+++ ptr.go
@@ -5,13 +5,15 @@ import (
 )
 
 type ptrSetter struct {
-	ptr    reflect.Value // The pointer to create/set on success.
-	tmp    reflect.Value // The value used by 'Value'.
-	setter Setter
+	ptr           reflect.Value // The pointer to create/set on success.
+	setterCreator SetterCreator
+	tag           reflect.StructTag
 }
 
 func (ps *ptrSetter) IsBoolFlag() bool {
-	if setter, ok := ps.setter.(interface{ IsBoolFlag() bool }); ok {
+	tmp := reflect.New(ps.setterCreator.Type()).Elem()
+	setter := ps.setterCreator.Setter(tmp, ps.tag)
+	if setter, ok := setter.(interface{ IsBoolFlag() bool }); ok {
 		return setter.IsBoolFlag()
 	}
 	return false
@@ -21,46 +23,51 @@ func (ps *ptrSetter) String() string {
 	if ps.ptr.Kind() == reflect.Invalid || ps.ptr.IsNil() {
 		return ""
 	}
-	return ps.setter.String()
+	return ps.setterCreator.Setter(ps.ptr.Elem(), ps.tag).String()
 }
 
 func (ps *ptrSetter) Set(s string) error {
-	if err := ps.setter.Set(s); err != nil {
+	tmp := reflect.New(ps.setterCreator.Type()).Elem()
+	if err := ps.setterCreator.Setter(tmp, ps.tag).Set(s); err != nil {
 		return err
 	}
-	ps.ptr.Set(ps.tmp.Addr())
+	ps.ptr.Set(tmp.Addr())
 	return nil
 }
 
 func (ps *ptrSetter) SetInt(i int64) error {
-	if err := ps.setter.SetInt(i); err != nil {
+	tmp := reflect.New(ps.setterCreator.Type()).Elem()
+	if err := ps.setterCreator.Setter(tmp, ps.tag).SetInt(i); err != nil {
 		return err
 	}
-	ps.ptr.Set(ps.tmp.Addr())
+	ps.ptr.Set(tmp.Addr())
 	return nil
 }
 
 func (ps *ptrSetter) SetUint(u uint64) error {
-	if err := ps.setter.SetUint(u); err != nil {
+	tmp := reflect.New(ps.setterCreator.Type()).Elem()
+	if err := ps.setterCreator.Setter(tmp, ps.tag).SetUint(u); err != nil {
 		return err
 	}
-	ps.ptr.Set(ps.tmp.Addr())
+	ps.ptr.Set(tmp.Addr())
 	return nil
 }
 
 func (ps *ptrSetter) SetFloat(f float64) error {
-	if err := ps.setter.SetFloat(f); err != nil {
+	tmp := reflect.New(ps.setterCreator.Type()).Elem()
+	if err := ps.setterCreator.Setter(tmp, ps.tag).SetFloat(f); err != nil {
 		return err
 	}
-	ps.ptr.Set(ps.tmp.Addr())
+	ps.ptr.Set(tmp.Addr())
 	return nil
 }
 
 func (ps *ptrSetter) SetBool(b bool) error {
-	if err := ps.setter.SetBool(b); err != nil {
+	tmp := reflect.New(ps.setterCreator.Type()).Elem()
+	if err := ps.setterCreator.Setter(tmp, ps.tag).SetBool(b); err != nil {
 		return err
 	}
-	ps.ptr.Set(ps.tmp.Addr())
+	ps.ptr.Set(tmp.Addr())
 	return nil
 }
 
@@ -83,11 +90,10 @@ func (psc *ptrSetterCreator) Setter(val reflect.Value, tag reflect.StructTag) Se
 	if val.Kind() != reflect.Ptr {
 		panic("value must be a pointer")
 	}
-	tmp := reflect.New(psc.setterCreator.Type()).Elem()
 	return &ptrSetter{
-		ptr:    val,
-		tmp:    tmp,
-		setter: psc.setterCreator.Setter(tmp, tag),
+		ptr:           val,
+		setterCreator: psc.setterCreator,
+		tag:           tag,
 	}
 }
 
